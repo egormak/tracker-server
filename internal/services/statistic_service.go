2289213ab366948b92c619a025ded8f2c8e8904a
Egor Maksimenko <egormaksimenko@gmail.com>
2025-01-22 16:15:17 +0200

    Created statistic service

--- internal/services/statistic_service.go
+++ internal/services/statistic_service.go
@@ -2,10 +2,21 @@ package services
 
 import "tracker-server/internal/domain/entity"
 
+type StatisticStorage interface {
+}
+
+type StatisticService struct {
+	st StatisticStorage
+}
+
+func NewStatisticService(st StatisticStorage) *StatisticService {
+	return &StatisticService{st: st}
+}
+
 // func (s *Service) StatCompletionTimeDone() ([]storage.TaskRecord, error) {
 // 	return s.srv.GetTaskRecordToday()
 // }
 
-func (s *Service) GetTaskRecordToday() ([]entity.TaskResult, error) {
+func (s *StatisticService) GetTaskRecordToday() ([]entity.TaskResult, error) {
 	return []entity.TaskResult{}, nil
 }
